48e345166716d849bb9582eaccca44c3a1b698e5
Nic Pillinger <nic@nic-Latitude-E6500.(none)>
2010-12-20 15:06:08 +0000

    initla commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leantasks::Application.config.secret_token = 'db72f613630e5481d54ac07b9e47e9d830fa8ab561b39ec98d7168af28cb77940b7e22a8f71e582444d0e6856579c1f0e41196aa0a0047fe04e2a231d34c9770'
